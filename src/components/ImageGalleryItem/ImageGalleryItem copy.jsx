772fffecaa8fe485b3a32e227cfc8ea6272b947f
10Artur <artyr11012002@gmail.com>
2023-08-26 21:38:32 +0300

    create copy

---
+++ src/components/ImageGalleryItem/ImageGalleryItem copy.jsx
@@ -0,0 +1,33 @@
+// import { Modal } from 'components/Modal/Modal';
+// import { Component } from 'react';
+// import {
+//   ImageGalleryItemCard,
+//   ImageGalleryItemCardImg,
+// } from './ImageGalleryItem.styled';
+
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isModal: false,
+//   };
+
+// handlerSwitchModal = () => {
+//   this.setState(({ isModal }) => ({ isModal: !isModal }));
+// };
+// render() {
+//   const {
+//     image: { webformatURL, largeImageURL, tags },
+//   } = this.props;
+//     return (
+//       <ImageGalleryItemCard onClick={this.handlerSwitchModal}>
+//         <ImageGalleryItemCardImg src={webformatURL} alt={tags} />
+//         {this.state.isModal && (
+//           <Modal
+//             src={largeImageURL}
+//             alt={tags}
+//             onClose={this.handlerSwitchModal}
+//           />
+//         )}
+//       </ImageGalleryItemCard>
+//     );
+// }
+// }
