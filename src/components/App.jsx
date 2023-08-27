cf2c11f47084adf6939739d8d967c9b66e65a567
10Artur <artyr11012002@gmail.com>
2023-08-27 21:04:41 +0300

    Refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,66 +2,65 @@ import { Button } from './Button/Button';
 import { Container, GlobalStyle } from './GlobalStyle';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
-import { Component } from 'react';
 import { pixabayAPI } from './API/API';
 import { Loader } from './Loader/Loader';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    query: '',
-    items: [],
-    page: 1,
-    perPage: 12,
-    totalHits: 0,
-    isLoading: false,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(12);
+  const [totalHits, setTotalHits] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { query, page, perPage } = this.state;
-    try {
-      if (prevState.query !== query || prevState.page !== page) {
-        this.setState({ isLoading: true });
+  useEffect(() => {
+    async function getImg() {
+      try {
+        setIsLoading(true);
         const resp = await pixabayAPI(query, page, perPage);
         if (resp.status !== 200) {
           throw new Error();
         }
-        this.setState(prevState => ({
-          items: [...prevState.items, ...resp.data.hits],
-          totalHits: resp.data.totalHits,
-          isLoading: false,
-        }));
+        setItems(prevItems => [...prevItems, ...resp.data.hits]);
+        setTotalHits(resp.data.totalHits);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
       }
-    } catch (error) {
-      console.log(error.message);
     }
-  }
+    if (query !== '') {
+      getImg();
+    }
+  }, [page, perPage, query]);
 
-  handleSearch = evt => {
+  const handleSearch = evt => {
     evt.preventDefault();
     const query = evt.target.elements.query.value.trim();
-    this.setState({ query, items: [], page: 1 });
+
+    setQuery(query);
+    setItems([]);
+    setPage(1);
     evt.target.reset();
   };
 
-  handleLoadMoreBtn = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleLoadMoreBtn = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  render() {
-    const { items, perPage, totalHits, page, isLoading } = this.state;
-    const totalPage = Math.round(totalHits / perPage);
+  const totalPage = Math.round(totalHits / perPage);
 
-    return (
-      <Container>
-        <Searchbar onSubmit={this.handleSearch} />
-        {isLoading && <Loader />}
-        <ImageGallery images={this.state.items} />
-        {items.length > 0 && page <= totalPage && (
-          <Button click={this.handleLoadMoreBtn} />
-        )}
+  return (
+    <Container>
+      <Searchbar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      <ImageGallery images={items} />
+      {items.length > 0 && page <= totalPage && (
+        <Button click={handleLoadMoreBtn} />
+      )}
 
-        <GlobalStyle />
-      </Container>
-    );
-  }
-}
+      <GlobalStyle />
+    </Container>
+  );
+};
